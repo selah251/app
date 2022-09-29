ca575556d8827580309470b2e20b65b8f0fd4f14
selah251 <zselahdin67@gmail.com>
2022-09-29 11:09:26 +0300

    React

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+          Hit a subscribe button
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Hello world
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
